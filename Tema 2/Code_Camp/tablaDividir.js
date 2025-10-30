55a473b5dda5ec3fdec612e9acfc9b746f8622b9
jose <josefol881@gmail.com>
2025-10-30 16:27:33 +0100

    Ejercicios Code Camp JavaScript

---
+++ Tema 2/Code_Camp/tablaDividir.js
@@ -0,0 +1,20 @@
+const mostrarTablaDividir = (event) => {
+    event.preventDefault();
+    const numero = Number(document.getElementById('numeroD').value);
+
+    if (numero >= 0 && numero <= 10) {
+        let tabla = document.getElementById('tablaD');
+        let tablaDividir = `<h2>Tabla de dividir del número ${numero}</h2>`;
+        tablaDividir += '<ul>';
+        
+        for (let i = 1; i <= 10; i++) {
+            tablaDividir += `<li>${numero} / ${i} = ${numero / i}</li>`;
+        }
+        
+        tablaDividir += '</ul>';
+        tabla.innerHTML = tablaDividir;
+    } else {
+        alert('El número introducido debe estar entre 0 y 10 (ambos inclusive)');
+        document.getElementById("numero").value = '';
+    }
+}
